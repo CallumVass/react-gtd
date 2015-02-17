8002fcbc0b45e50aba9700caa2600e2110de67f8
CallumVass <callum.vass@gmail.com>
2015-02-17 16:25:39 +0000

    Today

--- components/AppPage.jsx
+++ components/AppPage.jsx
@@ -1,7 +1,8 @@
 'use strict';
 
 var React = require('react'),
-    ViewSelector = require("./ViewSelector")
+    ViewSelector = require("./ViewSelector"),
+    SideNav = require("./SideNav")
     ;
 
 module.exports = React.createClass({
@@ -12,7 +13,19 @@ module.exports = React.createClass({
         return (
             <div className="row">
                 <div className="col-sm-3">
-                    <div>Hi</div>
+                    <SideNav title="Agenda"
+                        image="agenda"
+                        route="agenda" />
+                    <SideNav title="Inbox"
+                        image="inbox"
+                        route="inbox" />
+                    <SideNav title="Projects"
+                        image="projects"
+                        route="projects" />
+                    <SideNav title="Contexts" image="contexts" />
+                    <SideNav title="Starred"
+                        image="starred"
+                        route="starred" />
                 </div>
                 <div className="col-sm-9">
                     <ViewSelector />
--- components/SideNav.jsx
+++ components/SideNav.jsx
@@ -3,6 +3,10 @@
 var React = require("react");
 
 module.exports = React.createClass({
+    propTypes: {
+      title: React.PropTypes.string.isRequired,
+        image: React.PropTypes.oneOf(["agenda", "inbox", "contexts", "projects", "starred"])
+    },
 
     render: function () {
         return <div>Side Nav</div>;
